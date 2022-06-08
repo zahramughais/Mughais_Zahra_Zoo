4c39e50fff24fcba70d403aeab77bbf2839b8005
Zahra Mughais <zahramughais@gmail.com>
2022-06-08 13:33:08 +0300

    adding return self

--- zoo.py
+++ zoo.py
@@ -8,15 +8,13 @@ class Zoo:
         self.name = zoo_name
     def add_lion(self, name):
         self.animals.append( Lion(name) )
+        return self
     def add_tiger(self, name):
         self.animals.append( Tiger(name) )
+        return self
     def print_all_info(self):
         print("-"*30, self.name, "-"*30)
         for animal in self.animals:
             animal.display_info()
-zoo1 = Zoo("John's Zoo")
-zoo1.add_lion("Nala")
-zoo1.add_lion("Simba")
-zoo1.add_tiger("Rajah")
-zoo1.add_tiger("Shere Khan")
-zoo1.print_all_info()
+        return self
+zoo1 = Zoo("John's Zoo").add_lion("Nala").add_lion("Simba").add_tiger("Rajah").add_tiger("Shere Khan").print_all_info()
